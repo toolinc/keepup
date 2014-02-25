76db6b641acf67ac42c3bbe9e1c7b442f84f9923
Edgar Martinez Rico <edgar.martinez.rico@gmail.com>
2014-02-24 22:51:04 -0800

    Define the supported measurements

---
+++ keepUp-backEnd/src/main/java/com/udm/health/persistence/model/domain/InternationalSystemUnits.java
@@ -0,0 +1,31 @@
+// Copyright 2014 University of Detroit Mercy.
+
+package com.udm.health.persistence.model.domain;
+
+/**
+ * International System of Units.
+ *
+ * @author Oscar Rico (martinezr.oscar@gmail.com)
+ */
+public enum InternationalSystemUnits {
+
+    /**
+     * Millimetre of mercury
+     */
+    mmHg,
+
+    /**
+     * Kilogram
+     */
+    kg,
+
+    /**
+     * Meter
+     */
+    m,
+
+    /**
+     * Celsius
+     */
+    C
+}
---
+++ keepUp-backEnd/src/main/java/com/udm/health/persistence/validation/PositiveInteger.java
@@ -0,0 +1,41 @@
+// Copyright 2014 University of Detroit Mercy.
+
+package com.udm.health.persistence.validation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Validates that a given number is bigger than -1.
+ *
+ * @author Oscar Rico (martinezr.oscar@gmail.com)
+ */
+@Documented
+@NotNull
+@Min(value = 0)
+@ReportAsSingleViolation
+@Constraint(validatedBy = {})
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
+@Retention(RUNTIME)
+public @interface PositiveInteger {
+
+    String message() default "{com.udm.health.persistence.validation.PositiveInteger.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
